2f8ad388c3099a4737e02ce151ef3eaf0b07008f
Vinay kumar <vinayp2702@gmail.com>
2024-10-21 23:13:07 +0530

    all features added

--- spotify-clone/src/components/AlbumItem.jsx
+++ spotify-clone/src/components/AlbumItem.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import { useNavigate } from 'react-router-dom'
 function AlbumItem({image,name,desc,id}) {
     const navigate =useNavigate()
+  
   return (
     <div  onClick={()=>navigate(`/album/${id}`)} className='min-w-[100px] p-2 px-3 rounded cursor-pointer hover:bg-[#ffffff26'>
       <img className='rounded' src={image} alt="" />
